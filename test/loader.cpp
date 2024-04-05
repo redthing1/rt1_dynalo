307bc6a975a9c2f7dfc476c5e50da8fe3acf7c48
redthing1 <redthing1@alt.icu>
2024-04-05 11:41:04 -0700

    test: show message about usage

--- test/loader.cpp
+++ test/loader.cpp
@@ -2,14 +2,23 @@
 
 #include <cstdint>
 #include <sstream>
+#include <iostream>
 
 // usage: loader "path/to/shared/lib/dir"
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
-    dynalo::library lib(std::string(argv[1]) + "/" + dynalo::to_native_name("shared"));
+    std::cout << "loader" << std::endl;
+    if (argc < 2)
+    {
+        std::cerr << "usage: loader <path/to/shared/lib/dir>" << std::endl;
+        return 1;
+    }
+    auto shared_lib_path = std::string(argv[1]) + "/" + dynalo::to_native_name("shared");
+    std::cout << "loading shared library: " << shared_lib_path << std::endl;
+    dynalo::library lib(shared_lib_path);
 
-    auto add_integers  = lib.get_function<int32_t(const int32_t, const int32_t)>("add_integers");
-    auto print_message = lib.get_function<void(const char*)>("print_message");
+    auto add_integers = lib.get_function<int32_t(const int32_t, const int32_t)>("add_integers");
+    auto print_message = lib.get_function<void(const char *)>("print_message");
 
     std::ostringstream oss;
     oss << "it works: " << add_integers(1, 2);
